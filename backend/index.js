d5a2c950c2498f3ad01b899f6a3c45b354a70328
M4nxx14bbc0 <parrarodriguez.manue@studenti.salesianisesto.it>
2022-03-27 11:33:00 +0200

    ok

--- backend/index.js
+++ backend/index.js
@@ -41,33 +41,43 @@ apiServer.post("/api/insertNewElement",  (request, response)=>{
 
 apiServer.delete("/api/deleteElement",  (request, response)=>{
     console.log("Request: ", request.query);
-    conn.query(
-        'DELETE FROM test.magazzino WHERE codice=?;',
-        request.body.codice,
-        (err, result)=>{
-            console.log("Analysis: ", err);
-            response.setHeader("Content-Type", "application/json");
-            if(err==null)
-                response.status(200).json({message:'Successful Sign Up!'});
-            else
-                response.status(400).json({message:'Failed To Sign In!'});
+    var form = formidable({multiples:true});
+    form.parse(request,(error, fields)=>{
+        if(error){
+            console.log("Err", error);
+            response.status(400).json({message:'Update failed!'});
+            return null;
+        } else {
+            conn.query(
+                'DELETE FROM test.magazzino WHERE codice=?;',
+                request.body.codice,
+                (err, result)=>{
+                    console.log("Analysis: ", err);
+                    response.setHeader("Content-Type", "application/json");
+                    if(err==null)
+                        response.status(200).json({message:'Successful Sign Up!'});
+                    else
+                        response.status(400).json({message:'Failed To Sign In!'});
+                }
+            );
         }
-    );
+    });
 });
 
 apiServer.put("/api/updateElementNumber",  (request, response)=>{
     var form = formidable({multiples:true});
-    form.parse(request,(err, fields)=>{
-        if(err){
-            console.log("Err",err);
+    form.parse(request,(error, fields)=>{
+        if(error){
+            console.log("Err", error);
             response.status(400).json({message:'Update failed!'});
             return null;
         } else {
-            console.log("Richiesta a questo stronzo",fields);
-            conn.query(
-                'UPDATE test.magazzino SET quantita=? WHERE codice="?";',
-                [parseInt(fields.quantita), fields.codice],
-                (err)=>{
+            console.log("Richiesta a questo stronzo", fields);
+            var c = conn.query(
+                'UPDATE test.magazzino SET quantita = ? WHERE codice = ?;',
+                [fields.quantita, fields.codice],
+                (err, result, fields_my)=>{
+                    console.error("errore", err, c.sql, fields_my);
                     if(err)
                         response.status(400).json({message:'Update failed!'});
                     else
@@ -75,18 +85,25 @@ apiServer.put("/api/updateElementNumber",  (request, response)=>{
                 }
             );
         }
-    })
+    });
 });
 apiServer.get("/api/getAllElements",  (request, response)=>{
-    conn.query(
-        'SELECT * FROM test.magazzino',
-        (err, result)=>{
-            console.log(result);
-            response.setHeader("Content-Type", "application/json");
+    var mysql = "SELECT * FROM test.magazzino";
+    console.log(Object.keys(request.query).length);
+    if(Object.keys(request.query).length != 0) {
+        mysql += " WHERE codice = ?;";
+        conn.query(mysql, [request.query.codice],(err, result)=>{
             if(result)
                 response.status(200).json({data: result, message:'Successfully retrieved data'});
             else
                 response.status(400).json({message:'Can\'t retrieve data!'});
-        }
-    );
+        });
+    } else {
+        var c = conn.query(mysql, (err, result)=>{
+            if(result)
+                response.status(200).json({data: result, message:'Successfully retrieved data'});
+            else
+                response.status(400).json({message:'Can\'t retrieve data!'});
+        });
+    }
 });
